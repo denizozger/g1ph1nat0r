239995ee3de6ae80ed5c4d7cc7725069789c9a7a
Deniz Ozger <deniz@klarismo.com>
2016-09-25 21:38:23 +0100

    Added caching

--- index.js
+++ index.js
@@ -3,15 +3,13 @@
 global.libRequire = file => require(__dirname + '/lib/' + file);
 
 const 
-	express 					= require('express'),
-  app 							= express(),  
-  routes 						= libRequire('routes'),
-  port 							= process.env.PORT || 3000;
+  express = require('express'),
+  app     = express(),  
+  routes  = libRequire('routes'),
+  port    = process.env.PORT || 3000;
 
 module.exports = app;
 
 app.use('/', routes);
 
-app.listen(port, () => {
-  console.log(`Giphinator listening on port ${port}`);
-});
+app.listen(port, console.log(`Giphinator listening on port ${port}`));
--- test/test.js
+++ test/test.js
@@ -1,63 +1,116 @@
 'use strict'
 
 const
-	assert 	= require('assert'),
-	request = require('supertest'),
-	should 	= require('should'),
-	app 		= require('../index');
+  assert  = require('assert'),
+  request = require('supertest'),
+  should  = require('should'),
+  crypto  = require('crypto'),
+  app     = require('../index'),
+  giphy   = require('../lib/services/giphy'),
+  cache   = require('../lib/services/cache');
 
 /**
  * Router tests
  */
-describe('GET /:queryText', function() {
-
-  it('should return a gif when searched for "cat"', function(done) {
-    request(app)
-      .get('/cat')
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .expect(function(res) {
-      	should.exist(res.body.url);
-      })
-      .end(done);
-  });
-
-});
-
-/**
- * Integration tests
- */
-
-const
-	giphy 	= require('../lib/services/giphy'),
-	crypto 	= require('crypto');
-
 describe('Integration tests', function() {
 
-	describe('Giphy', function() {
+  describe('Routing tests', function() {
 
-	  it('should return a gif', function(done) {
-	    giphy.search('something')
-	    	.then(function(url) {
-	    		(url).should.be.a.String();
-	    		done();
-	    	})
-	    	.catch(console.error);
-	  });
+    describe('GET /:queryText', function() {
+      it('should return a gif when searched for "cat"', function(done) {
+        request(app)
+          .get('/cat')
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .expect(function(res) {
+            should.exist(res.body.url);
+          })
+          .end(done);
+      });
+    });
 
-	  // this is to test if the promise rejects the query properly
-	  it('should NOT return a gif', function() {
-	  	let randomCharacters = crypto.randomBytes(20).toString('hex');
+    describe('DELETE /:queryText', function() {
 
-	    return giphy.search(randomCharacters)
-	    	.then(function(url) {
-	    		if (url) {
-	    			throw new Error(`Found results for ${randomCharacters} (⊙_◎)`);
-	    		}
-	    	})
-	    	.catch(console.error);
-	  });
+      it('should remove the cached key', function(done) {
+        request(app)
+          .delete('/cat')
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .expect(function(res) {
+            should.exist(res.body.query);
+          })
+          .end(done);
+      });
+
+      let randomCharacters = crypto.randomBytes(20).toString('hex');
+
+      it(`should return 404 if key wasn't found`, function(done) {
+        request(app)
+          .delete('/randomCharacters')
+          .expect(404, done);
+      });
+
+    });
+
+    describe('GET /', function() {
+      it('should return 404', function(done) {
+        request(app)
+          .get('/')
+          .expect(404, done);
+      });
+    });
+
+  });
+
+  describe('Giphy tests', function() {
+
+    it('should return a gif', function(done) {
+      giphy.search('something')
+        .then(function(url) {
+          (url).should.be.a.String();
+          done();
+        });
+    });
+
+    // this is to test if the promise rejects the query properly
+    it('should NOT return a gif', function() {
+      let randomCharacters = crypto.randomBytes(20).toString('hex');
+
+      return giphy.search(randomCharacters)
+        .then(function(url) {
+          if (url) {
+            throw new Error(`Found results for ${randomCharacters} (⊙_◎)`);
+          }
+        });
+    });
+
+  });   
+
+  describe('Cache tests', function() {
+
+    let insertedGiphy;
+
+    it('should insert a record', function() {
+      return cache.set((new Date()).getTime(), 'some url')
+        .then(function(giphy) {
+          insertedGiphy = giphy;
+        });
+    });
+
+    it('should find a record by query', function() {
+      return cache.get(insertedGiphy.query)
+        .then(function(retrievedGiphy) {
+          should.exists(retrievedGiphy);
+          (retrievedGiphy.giphId).should.equal(insertedGiphy.giphId);
+        });
+    });
+
+    it('should delete a record by id', function() {
+      cache.delete(insertedGiphy.giphId)
+        .then()
+    });
 
   });
 
 });
+
